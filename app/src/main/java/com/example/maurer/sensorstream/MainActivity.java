ad91cf52f931ad02d01eb85f88e03392ddd0738b
SimonTheProgrammer <Simon.Maurer1@gmx.at>
2017-11-01 19:18:30 +0100

    no new changes made.

--- app/src/main/java/com/example/maurer/sensorstream/MainActivity.java
+++ app/src/main/java/com/example/maurer/sensorstream/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
                 board.tearDown(); //shuts down the board -> saves energy
             }
         });
-        //Yooo
 
         //Battery level Listener:
         findViewById(R.id.battery).setOnClickListener(new View.OnClickListener() {
